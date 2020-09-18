2248a8372820e8768dba49e72aa0336a173791ce
Aman <amanpant250@gmail.com>
2020-09-18 19:55:18 +0530

    Updates

---
+++ precache-manifest.4d4b127fd8215f3bffe2717a78e49020.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0505dbfa98ebce17eef2fdb08880bfc",
+    "url": "/reactliveweb/index.html"
+  },
+  {
+    "revision": "559ce9faba1e2d07b0f1",
+    "url": "/reactliveweb/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "ba4e560d9735bee6c1c4",
+    "url": "/reactliveweb/static/css/main.17846e18.chunk.css"
+  },
+  {
+    "revision": "559ce9faba1e2d07b0f1",
+    "url": "/reactliveweb/static/js/2.bcc794cf.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactliveweb/static/js/2.bcc794cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4e560d9735bee6c1c4",
+    "url": "/reactliveweb/static/js/main.6f1e2277.chunk.js"
+  },
+  {
+    "revision": "d09addabd1d96ef6087e",
+    "url": "/reactliveweb/static/js/runtime-main.0ef07212.js"
+  },
+  {
+    "revision": "e3febbc876e5315b370081e2d4dbb5e1",
+    "url": "/reactliveweb/static/media/andriod.e3febbc8.jpg"
+  },
+  {
+    "revision": "7a5ecbaa9ecd0a6b485f015b818795ed",
+    "url": "/reactliveweb/static/media/app.7a5ecbaa.jpg"
+  },
+  {
+    "revision": "f1e603cc9f4dad084a1849c09d08142e",
+    "url": "/reactliveweb/static/media/digital.f1e603cc.jpg"
+  },
+  {
+    "revision": "b353d65799902d79566163581ba43a00",
+    "url": "/reactliveweb/static/media/i1.b353d657.gif"
+  },
+  {
+    "revision": "e35df7b3f2bad9cf819a73da5fcf42f8",
+    "url": "/reactliveweb/static/media/i2.e35df7b3.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactliveweb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "20c6ac0ff86e1155a20c482f1c316829",
+    "url": "/reactliveweb/static/media/marketing.20c6ac0f.jpg"
+  },
+  {
+    "revision": "8e728f5d084d71275fb24b81d526b54e",
+    "url": "/reactliveweb/static/media/software.8e728f5d.jpg"
+  },
+  {
+    "revision": "eedd2938b91525013b90b2ac5dec217b",
+    "url": "/reactliveweb/static/media/web.eedd2938.jpeg"
+  }
+]);
